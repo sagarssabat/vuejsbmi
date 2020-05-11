92e82818b72350a6f06821cf5b85006e1848b9a0
SagarSabat <sagarssabat@gmail.com>
2020-05-11 12:58:59 +0530

    attribute binding

--- js/product.js
+++ js/product.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image: "./assets/images/vmSocks-green-onWhite.jpg",
+        link: "https://vuejs.org/v2/guide/",
     }
 })
